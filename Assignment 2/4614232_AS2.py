aca2e704d094aa7ad525bf591d8c21fe957994a2
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-05-08 14:57:43 +0200

    Commented code

--- Assignment 2/4614232_AS2.py
+++ Assignment 2/4614232_AS2.py
@@ -1,28 +1,32 @@
+# Import necessary libraries
 import csv
 from typing import List
 import numpy as np
 
 
+# Define a class for points in the 2D plane
 class Point:
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
+    # Print the point coordinates
     def print(self):
         print("{0} {1}".format(self.x, self.y))
 
+    # Returns a list of elements for the conic equation
     def coniceq(self):
         return [self.y**2, self.x * self.y, self.x, self.y, -1]
 
+    # Returns the square of the x-coordinate for the conic equation
     def conicsq(self):
         return -self.x**2
 
 
+# Read input points from CSV file
 inputarray = []
-
 with open(r"input as2.csv", "r") as input:
     locations = csv.DictReader(input)
-
     for pointentry in locations:
         point = Point(pointentry["x"], pointentry["y"])
         inputarray.append(point)
@@ -33,7 +37,7 @@ numOfRows = calcarray.shape[0]
 sol = []
 eq = []
 
-
+# Check if there are exactly five points
 if numOfRows == 5:
     print("Given five points a circle will be calculated")
     for n in range(5):
@@ -41,37 +45,20 @@ if numOfRows == 5:
         sol.append(inputarray[n].conicsq())
     eqarray = np.array(eq)
     solarray = np.array(sol)
+    # Solve the linear system of equations
     solution = np.linalg.solve(eqarray, solarray)
-    if solution[0] < 0:
-        pr_sol0 = str(solution[0])
-    else:
-        pr_sol0 = "+" + str(solution[0])
-    if solution[1] < 0:
-        pr_sol1 = str(solution[1])
-    else:
-        pr_sol1 = "+" + str(solution[1])
-    if solution[2] < 0:
-        pr_sol2 = str(solution[2])
-    else:
-        pr_sol2 = "+" + str(solution[2])
-    if solution[3] < 0:
-        pr_sol3 = str(solution[3])
-    else:
-        pr_sol3 = "+" + str(solution[3])
-    if solution[4] < 0:
-        pr_sol4 = str(solution[4])
-    else:
-        pr_sol4 = abs(solution[4])
 
-Equation = [
-    "Equation:",
-    "x^2{0}y^2{1}xy{2}x{3}y = 0".format(pr_sol0, pr_sol1, pr_sol2, pr_sol3, pr_sol4),
-]
-print(Equation[0], Equation[1])
+    # Format the coefficients of the circle equation
+    pr_solutions = [str(x) if x < 0 else "+" + str(x) for x in solution]
 
-f = open("out AS2.csv", "w")
+    # Create the circle equation string
+    Equation = [
+        "Equation:",
+        "x^2{0}y^2{1}xy{2}x{3}y = 0".format(*pr_solutions),
+    ]
+    print(Equation[0], Equation[1])
 
-writer = csv.writer(f)
-writer.writerow(Equation)
-
-f.close()
+    # Write the circle equation to the output CSV file
+    with open("out AS2.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(Equation)
