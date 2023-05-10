716246ca50caf1deb91422dc59360ef192777df4
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-05-10 18:44:48 +0200

    Almost got it

--- Assignment 3/4614232_AS3.py
+++ Assignment 3/4614232_AS3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 
 t = np.array([0.025, 0.05, 0.075, 0.1, 0.125])  # time(x-axis)
 u = np.array([8.57, 11.01, 11.78, 11.90, 11.98])  # Voltage(y-axis)
@@ -17,19 +18,40 @@ u = np.array([8.57, 11.01, 11.78, 11.90, 11.98])  # Voltage(y-axis)
 # plt.show()
 
 # calculate the sum of t*u, sum of t, sum of u, sum of t^2, and i
-sumtu = np.sum(t * u)
-sumt = np.sum(t)
-sumu = np.sum(u)
-sumt2 = np.sum(t**2)
-i = len(t)
+# sumtu = np.sum(t * u)
+# sumt = np.sum(t)
+# sumu = np.sum(u)
+# sumt2 = np.sum(t**2)
+# i = len(t)
+#
+# B = (i * sumtu - sumt * sumu) / (i * sumt2 - sumt * sumu)  # calculate B constant
+# A1 = (1 / i) * (sumu - B * sumt)  # calculate A constant
+# A = np.exp(A1)  # calculate A constant
+## plot the data and the fitted line
+# plt.plot(t, u, "o", label="Original data", markersize=10)
+## plot the exponential function U=e^A*e^Bt
+# plt.plot(t, A * np.exp(B * t), "r", label="Fitted line")
+## print the exponential function above the graph
+# plt.text(0.05, 13, "U = {0}*e^{1}t".format(A, B))
+# plt.legend()
+# plt.show()
 
-A = (i * sumtu - sumt * sumu) / (i * sumt2 - sumt * sumu)  # calculate A constant
-B = (1 / i) * (sumu - A * sumt)  # calculate B constant
-# plot the data and the fitted line
+# A, B = np.polyfit(t, np.log(u), 1, w=np.sqrt(u))
+### plot the data and the fitted line
+# plt.plot(t, u, "o", label="Original data", markersize=10)
+### plot the exponential function U=e^A*e^Bt
+# plt.plot(t, np.exp(A) * np.exp(B * t), "r", label="Fitted line")
+### print the exponential function above the graph
+# plt.text(0.05, 13, "U = {0}*e^{1}t".format(A, B))
+# plt.legend()
+# plt.show()
+x, y = opt.curve_fit(lambda t, A, B: A * np.exp(B * t), t, u, p0=(4, 0.1))
+A, B = x
+### plot the data and the fitted line
 plt.plot(t, u, "o", label="Original data", markersize=10)
-# plot the exponential function U=e^A*e^Bt
-plt.plot(t, np.exp(A) * np.exp(B * t), "r", label="Fitted line")
-# print the exponential function above the graph
-plt.text(0.05, 13, "U = e^{0}*e^{1}t".format(A, B))
+### plot the exponential function U=e^A*e^Bt
+plt.plot(t, A * np.exp(B * t), "r", label="Fitted line")
+### print the exponential function above the graph
+plt.text(0.05, 13, "U = {0}*e^{1}t".format(A, B))
 plt.legend()
 plt.show()
