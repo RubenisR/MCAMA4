2d17b6a33a0604f02d2f59cc5848c3239e3cbc63
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2023-05-11 16:44:40 +0200

    fixed correct

--- Assignment 3/test.py
+++ Assignment 3/test.py
@@ -2,25 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
-# Define the time and voltage data
-t = np.array([0.025, 0.05, 0.075, 0.1, 0.125])  # time(x-axis)
+t = np.array([0.025, 0.05, 0.075, 0.1, 0.125])  # time
 u = np.array([3.43, 0.99, 0.22, 0.10, 0.02])  # Voltage
 
 
 def expon_func(t, A, B):
-    return A * np.exp(B * t)
+    return A * np.exp(-t / B)
 
 
 x, y = opt.curve_fit(expon_func, t, u, p0=(12, 0.05))
 
-# Calculate the best fit for UC(t) using the optimized parameter values
 UC = expon_func(t, *x)
-# Calculate RC
-RC = 1 / abs(x[1])
 
-print("RC =", RC)
-
-# Plot the data and the fitted function
 plt.plot(t, u, "ro", label="Data")
 plt.plot(t, UC, "b-", label="Fitted function")
 plt.xlabel("t")
